6b20de427e4371b566e1279782b0fb913aeca4e0
Pedro Avelino <pedro_avelino_@hotmail.com>
2025-11-07 19:48:59 -0300

    feat(app): Developing scrolling functionality on the task list screen.

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { FokusButton } from '../../components/FokusButton';
 import { IconPlus } from '../../components/Icons';
 import TaskItem from '../../components/TaskItem';
@@ -12,26 +12,35 @@ export default function Tasks () {
     return ( 
       <View style={styles.container}>
         <View style={styles.wrapper}>
-          <Text style={styles.text}>
-          Lista de tarefas:
-        </Text>
-        <View style={styles.inner}>
-          {tasks.map(t => {
-            return (
-              <TaskItem 
-                completed={t.completed}
-                text={t.description}
-                key={t.id}
-              />
-            )
-          })}
-        </View>
-        <FokusButton 
-          title="Adicionar nova tarefa" 
-          icon={<IconPlus outline={true}/>}
-          outline
-          onPress={() => router.navigate("/add-task")}
-        />
+          <View style={styles.inner}>
+            <FlatList 
+              data={tasks}
+              renderItem={({ item }) => <TaskItem 
+                completed={item.completed}
+                text={item.description}
+                key={item.id}
+              />}
+              keyExtractor={item => item.id}
+              ItemSeparatorComponent={() => 
+                <View style={{ height: 8 }}
+              />}
+              ListHeaderComponent={
+                <Text style={styles.text}>
+                  Lista de tarefas:
+                </Text>
+              }
+              ListFooterComponent={
+                <View style={{ marginTop: 16}}>
+                  <FokusButton 
+                    title="Adicionar nova tarefa" 
+                    icon={<IconPlus outline={true}/>}
+                    outline
+                    onPress={() => router.navigate("/add-task")}
+                  />
+                </View>
+              }
+            />
+          </View>
         </View>
       </View>
     )
@@ -51,6 +60,7 @@ const styles = StyleSheet.create ({
     textAlign: "center",
     color: "#FFF",
     fontSize: 26,
+    marginBottom: 16,
   },
   inner: {
     gap: 8,
