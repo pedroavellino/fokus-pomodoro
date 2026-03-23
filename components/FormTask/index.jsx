95ce93a5d35b938e400c3c8195c42979f444784a
Pedro Avelino <pedro_avelino_@hotmail.com>
2026-03-23 19:19:31 -0300

    style(form-task): adjusts the position and size of the form task title

--- components/FormTask/index.jsx
+++ components/FormTask/index.jsx
@@ -42,32 +42,34 @@ export default function FormTask({
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
-          <Text style={styles.text}>
-            {defaultValue ? "Editar uma tarefa:" : "Adicionar uma tarefa:"}
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            {defaultValue ? "Editar uma tarefa" : "Adicionar uma tarefa"}
           </Text>
-          <Text style={styles.label}>Em que você está trabalhando?</Text>
-          <TextInput 
-            style={styles.input}
-            numberOfLines={10}
-            multiline={true}
-            value={description}
-            onChangeText={setDescription}
-          />
-          <View style={styles.action}>
-            {onDeleteTask && (
+          <View style={styles.inner}>
+            <Text style={styles.label}>Em que você está trabalhando?</Text>
+            <TextInput 
+              style={styles.input}
+              numberOfLines={10}
+              multiline={true}
+              value={description}
+              onChangeText={setDescription}
+            />
+            <View style={styles.action}>
+              {onDeleteTask && (
+                <View>
+                  <Pressable style={styles.button} onPress={deleteTask}>
+                    <IconDelete />
+                    <Text>Deletar</Text>
+                  </Pressable>
+                </View>
+              )}
               <View>
-                <Pressable style={styles.button} onPress={deleteTask}>
-                  <IconDelete />
-                  <Text>Deletar</Text>
+                <Pressable style={styles.button} onPress={submitTask}>
+                  <IconSave />
+                  <Text>Salvar</Text>
                 </Pressable>
               </View>
-            )}
-            <View>
-              <Pressable style={styles.button} onPress={submitTask}>
-                <IconSave />
-                <Text>Salvar</Text>
-              </Pressable>
             </View>
           </View>
         </View>
@@ -80,24 +82,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#021123",
-    gap: 16,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  content: {
+    marginTop: 40,
+    width: "90%",
+    gap: 16,
   },
   inner: {
     backgroundColor: "#98A0A8",
-    width: "90%",
-    height: 310,
     borderRadius: 8,
     padding: 16,
     gap: 32,
-
   },
-  text: {
+  title: {
+    textAlign: "center",
     color: "#FFF",
-    width: "90%",
-    borderRadius: 8,
-    gap: 32,
+    fontSize: 32,
+    marginBottom: 40,
   },
   label: {
     fontWeight: 600,
